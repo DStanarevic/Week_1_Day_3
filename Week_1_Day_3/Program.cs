e9ceef171737918b99275e3727332173fea367ce
DStanarevic <DejanStanarevic@gmail.com>
2017-09-27 12:02:10 -0400

    Conditional statements

--- Week_1_Day_3/Program.cs
+++ Week_1_Day_3/Program.cs
@@ -10,6 +10,40 @@ namespace Week_1_Day_3
     {
         static void Main(string[] args)
         {
+            ////Conditinonal statements. It's a way for a computer to handle multiple scenarios.
+            //int firstNumber = 2;
+            //int secondNumber = 6;
+
+            ////If else statement.
+            //if (firstNumber > secondNumber)
+            //{
+            //    //Do something
+            //    Console.WriteLine("You have broken math. Good Job");
+
+            //}
+            //else
+            //{
+            //    //
+            //    Console.WriteLine("That makes sense.");
+            //}
+
+            
+            Console.WriteLine("Please enter the day of the week.");
+            string day = Console.ReadLine();
+            if (day == "Monday")
+            {
+                Console.WriteLine("That sucks bruh.");
+            }
+            else if (day == "Friday")
+            {
+                Console.WriteLine("Partiezeit!");
+            }
+            else
+            {
+                Console.WriteLine("Weekend!");
+            }
+
+
         }
     }
 }
